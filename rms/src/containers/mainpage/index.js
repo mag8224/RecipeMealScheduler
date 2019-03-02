cfb120fa24b9c328591cbbf58411d6ce7ecfd02a
Melissa Gould <mag8224@g.rit.edu>
2019-03-02 12:44:34 -0500

    Added Container for the mainpage as an example

---
+++ rms/src/containers/mainpage/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class MainPage extends React.Component {
+    render() {
+      return (
+        <div >
+          <h1>Recipe Meal Scheduler</h1>
+          
+        </div>
+      );
+    }
+  }
+  
+export default MainPage;
