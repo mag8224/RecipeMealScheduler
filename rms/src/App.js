1bd0534d95361bf298dab8b33943541d70dfc75b
Melissa Gould <mag8224@g.rit.edu>
2019-03-05 11:47:01 -0500

    Added semantic-ui-react

--- rms/src/App.js
+++ rms/src/App.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 import MainPage from './containers/mainpage';
 
 class App extends Component {
--- rms/src/containers/mainpage/index.js
+++ rms/src/containers/mainpage/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+//import ReactDOM from 'react-dom';
+import { Button } from 'semantic-ui-react';
 
 class MainPage extends React.Component {
     render() {
       return (
         <div >
           <h1>Recipe Meal Scheduler</h1>
-          
+          <Button>Hello World</Button>
         </div>
       );
     }
