15ee5f386b7e482bc1889b480f31ff7cc9d1af80
Melissa Gould <mag8224@g.rit.edu>
2019-03-29 00:28:15 -0400

    adding ingredients

--- rms/src/components/AddRecipe/index.js
+++ rms/src/components/AddRecipe/index.js
@@ -1,50 +1,13 @@
 import React from 'react';
 //import ReactDOM from 'react-dom';
-import { Table, Input, List, Dropdown } from 'semantic-ui-react';
+import { Table, Input, List,  TextArea, Button } from 'semantic-ui-react';
 
 
-let measurements = [
-    {
-        key: "oz",
-        text: "oz",
-        value: "oz"
-    },
-    {
-        key: "C",
-        text: "Cups",
-        value: "C"
-    },
-    {
-        key: "tsp",
-        text: "tsp.",
-        value: "tsp"
-    },
-    {
-        key: "Tbsp",
-        text: "Tbsp",
-        value: "Tbsp"
-    },
-    {
-        key: "lbs",
-        text: "lbs",
-        value: "lbs"
-    },
-    {
-        key: "g",
-        text: "grams",
-        value: "g"
-    },
-    {
-        key: "kg",
-        text: "kg",
-        value: "kg"
-    },
-
-];
 
 class AddRecipe extends React.Component {
     constructor(props) {
         super(props);
+        
         this.state = {
             title: "",
             serving: 0,
@@ -52,17 +15,78 @@ class AddRecipe extends React.Component {
             ingredients: [],
             steps: [],
             notes: "",
+            timeError: false,
+            step: "",
+            tmpIng: {
+                name: "",
+                amt: "",
+                measure: ""
+            }
+        }
+        this.changeHandler = this.changeHandler.bind(this);
+        this.errorChecker = this.errorChecker.bind(this);
+        this.displaySteps = this.displaySteps.bind(this);
+        this.displayIngredients = this.displayIngredients.bind(this);
+
+    }
+    displaySteps() {
+
+    }
+    
+    displayIngredients() {
+        return this.state.ingredients.map(i => <Table.Row><Table.Cell>{i.name}</Table.Cell><Table.Cell><Button circular secondary icon="trash"></Button><Button circular primary icon="edit"></Button></Table.Cell></Table.Row>)
+    }
+
+    changeHandler(e) {
+        console.log(e.target);
+        if(this.errorChecker(e.target.value, e.target.name) === true) {
+            this.setState({
+                [e.target.name]:e.target.value
+            })
         }
     }
+    errorChecker(val, tar) {
+        if (tar === "time") {
+            if(val.length !== 5){
+                this.setState({timeError:true});
+                return false;
+            }
+            else if(!val.includes(":")) {
+                this.setState({timeError: true});
+                return false;
+            } 
+            else {
+                let b = true;
+                for(let i = 0; i<val.length; i++) {
+                    console.log(val.charAt(i), isNaN(val.charAt(i)), val.charAt(i) !== ":" )
+                    if(isNaN(val.charAt(i)) && val.charAt(i) !== ":")
+                    {
+                        b = false;
+                    }
+                }
+                if (b === false) {
+                    this.setState({timeError: true});
+                    return false;
+                }
+                this.setState({timeError: false});
+                return b;
+
+            }
+
+        }
+        this.setState({timeError: false});
+        return true;
+    }
+
     render() {
       return (
         <div >
           <h1>New Recipe</h1>
-            <div>
-                {/* Title, Serving size, Time*/}
-                <Input label="Title" placeholder="Recipe Title"></Input>
-                <Input label="Serving Size" type="number"></Input>
-                <Input label="Time" placeholder="HH:MM"></Input>
+            <div >
+                
+                <Input style={{padding: "10px"}} name="title" label="Title" placeholder="Recipe Title" onChange={this.changeHandler}></Input>
+                <Input style={{padding: "10px"}} name="serving" label="Serving Size" type="number" onChange={this.changeHandler}></Input>
+                <Input error={this.state.timeError} style={{padding: "10px"}} label="Time" name="time" placeholder="HH:MM" onChange={this.changeHandler}></Input>
             </div>
             <div>
                 {/**Ingredients */}
@@ -75,20 +99,45 @@ class AddRecipe extends React.Component {
                     <Table.Body>
                         <Table.Row>
                             <Table.Cell>
-                            <Input label="Amt" type="number"/>
+                            <Input ref="amount" type="number"  onChange={(e) => {
+                                let tmp = this.state.tmpIng;
+                                tmp.amt=e.target.value;
+                                this.setState({tmpIng: tmp});
+                            }}> 
+                            <input />
+                            <select  onChange={(e) => {
+                                let tmp = this.state.tmpIng;
+                                tmp.measure=e.target.value;
+                                this.setState({tmpIng: tmp});
+                              }}>
+                                <option value="tsp">tsp</option>
+                                <option value="Tbsp">Tbsp</option>
+                                <option value="C">C</option>
+                                <option value="oz">oz</option>
+                                <option value="lbs">lbs</option>
+                                <option value="g">grams</option>
+                                <option value="kg">kg</option>
+                            </select>
+                            </Input>
                             </Table.Cell>
+                          
                             <Table.Cell>
-                            <Dropdown
-                                placeholder="Measurement"
-                                fluid
-                                selection
-                                options={measurements}
-                                />
-                            </Table.Cell>
-                            <Table.Cell>
-                            <Input placeholder="Ingredient" />
+                                
+                            <Input placeholder="Ingredient"  onChange={(e) => {
+                                    let tmp = this.state.tmpIng;
+                                    tmp.name=e.target.value;
+                                    this.setState({tmpIng: tmp});
+                                }} >
+                            <input />
+                            <Button onClick={() => {
+                                let tmpArray = this.state.ingredients.slice();
+                                tmpArray.push(this.state.tmpIng);          
+                                this.setState({ingredients: tmpArray, tmpIng: {}});
+                            }}>Add</Button>
+                            </Input>
                             </Table.Cell>
                         </Table.Row>
+                        {this.displayIngredients}
                     </Table.Body>
                 </Table>
             </div>
@@ -102,19 +151,31 @@ class AddRecipe extends React.Component {
                     </Table.Header>
                     <Table.Body>
                         <Table.Row>
-                            <Table.Cell><Input placeholder="Add a Step"></Input></Table.Cell>
+                            {/**Add some kind of action here */}
+                            <Table.Cell><Input style={{width:"100%"}} name="step" placeholder="Add a Step" onChange={this.changeHandler}>
+                            <input />
+                            <Button onClick={() => {
+                                let tmpArray = this.state.steps.slice();
+                                tmpArray.push(this.state.step);
+                                this.setState({steps: tmpArray, step: ""});
+                                
+                            }}>Add</Button>
+                            </Input> </Table.Cell>
                         </Table.Row>
                         <Table.Row>
                             <Table.Cell><List as="ol">
                                 <List.Item as="li" value="1">Step directions blah blah blah blah blah</List.Item>
                             </List></Table.Cell>
+                            <Table.Cell><Button circular negative icon="trash"></Button><Button circular icon="edit" primary color="blue"></Button></Table.Cell>
                         </Table.Row>
                     </Table.Body>
                 </Table>
             </div>
-            <div>
+            <div style={{marginTop:"15px"}}>
                 {/**Additional Notes */}
+                <TextArea rows={5} style={{width:"70%"}} placeholder="Additional Notes" />
             </div>
+            <Button onClick={() =>console.log(this.state)} positive>Submit</Button>
         </div>
       );
     }
